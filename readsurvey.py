1a5677f0858c3c3d6408361d14063f3274fe74e4
greghell <greg.hellbourg@gmail.com>
2022-01-21 16:49:43 -0800

    Update readsurvey.py

--- readsurvey.py
+++ readsurvey.py
@@ -1,14 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+quick look at data files for one given timestamp (as given in file name)
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-fname = 'C:\\Users\\gregh\\Desktop\\siglent_DSA_RFI_survey\\survey_220113162522_0.016666666666666666h.txt';
-f = open(fname, "r")
-tmp = f.readlines();
-f.close();
+tstamp = '220120142412'
+fname1 = 'D:\\big_smoky_valley\\RFI1_survey_'+tstamp+'_1.0h.csv';
+fname2 = 'D:\\big_smoky_valley\\RFI2_survey_'+tstamp+'_1.0h.csv';
+fname3 = 'D:\\big_smoky_valley\\RFI3_survey_'+tstamp+'_1.0h.csv';
 
-numspec = len(tmp);
-wf = np.zeros((numspec,750));
-for k in range(numspec):
-    wf[k,:] = list(map(float,tmp[k].split(',')[1:-1]));
+with open(fname1) as file_name:
+    array1 = np.loadtxt(file_name, delimiter=",");
+with open(fname2) as file_name:
+    array2 = np.loadtxt(file_name, delimiter=",")
+with open(fname3) as file_name:
+    array3 = np.loadtxt(file_name, delimiter=",")
 
-plt.imshow(wf,aspect='auto');plt.show()
+plt.figure();
+plt.subplot(221);
+plt.imshow(array1[:,:-1],aspect='auto');
+plt.title('RFI#1');
+plt.subplot(222);
+plt.imshow(array2[:,:-1],aspect='auto');
+plt.title('RFI#2');
+plt.subplot(223);
+plt.imshow(array3[:,:-1],aspect='auto');
+plt.title('RFI#3');
+plt.show();
+
+plt.figure();
+plt.subplot(221);
+plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array1[:,:-1],axis=0));
+plt.grid('on');
+plt.title('RFI#1');
+plt.subplot(222);
+plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array2[:,:-1],axis=0));
+plt.grid('on');
+plt.title('RFI#2');
+plt.subplot(223);
+plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array3[:,:-1],axis=0));
+plt.grid('on');
+plt.title('RFI#3');
+plt.show();
+
+
+# plt.figure();
+# plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array1[:27,:-1],axis=0))
+# plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array1[40:40+27,:-1],axis=0))
+# plt.plot(np.linspace(0.5,3.2,array1.shape[1]-1),np.mean(array1[110:110+27,:-1],axis=0))
+# plt.show()
